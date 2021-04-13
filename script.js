adde4c6a21390832ae687dc24ffee7867b3bd4a1
anjali <anjalidinesh97@gmail.com>
2021-04-13 12:26:25 +0530

    Added stopwatch functionality

--- script.js
+++ script.js
@@ -13,9 +13,14 @@ function addItem()
         ol.appendChild(li);
         li.innerHTML=` <div class="card"><div class="card-body">
                             <h5 class="card-title">${itemValue}</h5>
+                            <div class="stopwatch-container">
+                            <a id="stopwatch-btn" onclick="startStop()"><i id="start-btn" class="fa fa-play" aria-hidden="true"></i></a>
+                            <span id="display-stopwatch">00:00:00</span>
+                            </div>
                             <a class="btn btn-default"></a>
                             </div></div>`;
     }
+ 
     
     
     let itemField=document.getElementById("input-item"); //make input field empty after adding item
@@ -41,4 +46,89 @@ function removeList()
     var olElement = document.getElementById('list-items');
     olElement.innerHTML = '';
 }
+
+//Stopwtach functionality start
+
+//define variable to hold h/m/s
+    let second=0;
+    let minute=0;
+    let hour=0;
+
+//define a variable to hold setinterval fun
+let interval=null;
+
+//define a variable to hold stopwatch status
+let status="stopped";
+
+//define variables to hold display values hh/mm/ss
+    let displaySecond=0;
+    let displayMinute=0;
+    let displayHour=0;
+
+function stopwatch()
+{
+    //increment second
+    second++;
+
+    //set a condition to increment minute and hour as well
+    if(second/60===1)
+    {
+        second=0;
+        minute++;
+        if(minute/60===1)
+        {
+            minute=0;
+            hour++;
+        }
+    }
+
+    //If the h, m, s has only one digit, add a zero before the value (00:02:09)
+    if(second<10)
+    {
+        displaySecond="0"+second;
+    }
+    else
+    {
+        displaySecond=second;
+    }
+    if(minute<10)
+    {
+        displayMinute="0"+minute;
+    }
+    else
+    {
+        displayMinute=minute;
+    }
+    if(hour<10)
+    {
+        displayHour="0"+hour;
+    }
+    else
+    {
+        displayHour=hour;
+    }
+
+
+    //display stopwatch
+    document.getElementById("display-stopwatch").innerHTML=displayHour + ":" + displayMinute + ":" + displaySecond; 
+}
+
+// function for start and stop the stop watch
+function startStop()
+{
+    if(status=="stopped")
+    {
+        //start the stopwatch
+        interval=window.setInterval(stopwatch,1000);
+        document.getElementById("start-btn").className="fa fa-pause";
+        status="started";
+    }
+    else{
+        window.clearInterval(interval);
+        document.getElementById("start-btn").className="fa fa-play";
+        status="stopped";
+    }
+
+}
+
 
